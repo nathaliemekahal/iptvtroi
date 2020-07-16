69b6a1bd8abe89b5e16b1f4f59e85a002910e3d7
Nathalie <nathaliemekahal@gmail.com>
2020-07-16 19:25:07 +0200

    wireframe

---
+++ src/components/Content.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import {Row,Col,Container} from 'react-bootstrap'
+import plansData from './data/data.json'
+
+class Content extends Component {
+  render() {
+      return (
+
+        <Container>
+            {console.log(plansData)}
+        <Row className='RowContainer-class row-cols-xs-2 row-cols-md-4'>
+            {plansData&&plansData.map(
+                Plan=><Col className='Col-class'>
+                {<div className='planwrapper_title'>
+                  <h4 className='h3_class'>{Plan.title}</h4>
+                </div>}
+                <div className='featureswrapper-class'>
+                    {Plan.features.map(feature=>
+                      <h6>{feature}</h6>
+                      )}
+                </div>
+              </Col>)}
+      
+        </Row>
+        </Container>
+      )
+    }
+}
+
+export default Content

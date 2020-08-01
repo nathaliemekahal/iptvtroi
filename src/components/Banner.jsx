70be14feed200630e7e9643f0a096dad83d3b0e8
Nathalie <nathaliemekahal@gmail.com>
2020-08-01 23:51:32 +0200

    fixing highlights

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,10 +3,21 @@ import React, { Component } from "react";
 class Banner extends Component {
   render() {
     return (
-      <div className="Heading-class">
-        {this.props.svgFile}
-        <h4 className="title-class">{this.props.title}</h4>
-      </div>
+      <>
+        {this.props.title === "Highlights" && (
+          <div className="Heading-class" style={{ paddingTop: "3rem" }}>
+            {this.props.svgFile}
+            <h4 className="title-class">{this.props.title}</h4>
+            {console.log("HEHEEH")}
+          </div>
+        )}
+        {this.props.title !== "Highlights" && (
+          <div className="Heading-class">
+            {this.props.svgFile}
+            <h4 className="title-class">{this.props.title}</h4>
+          </div>
+        )}
+      </>
     );
   }
 }

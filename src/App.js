80b862859bf2e6a0f476785ce9b660328a7e8ae5
Nathalie <nathaliemekahal@gmail.com>
2020-08-02 00:16:32 +0200

    contact us

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import "react-multi-carousel/lib/styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import banner from "./components/Banner";
 import Banner from "./components/Banner";
+import ContactUs from "./components/ContactUs";
 
 function App() {
   return (
@@ -51,6 +52,26 @@ function App() {
         title={"Plans"}
       />
       <Content />
+
+      <Banner
+        svgFile={
+          <svg
+            width="1em"
+            height="1em"
+            viewBox="0 0 16 16"
+            class="bi bi-person-lines-fill"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm7 1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm2 9a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5z"
+            />
+          </svg>
+        }
+        title={"Contact Us "}
+      />
+      <ContactUs />
     </div>
   );
 }
---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { Container, Row } from "react-bootstrap";
+class ContactUs extends Component {
+  render() {
+    return (
+      <>
+        <Container className="">
+          <Row>
+            <h5 className="Contact-heading">Email: </h5>
+            <p></p>
+          </Row>
+        </Container>
+      </>
+    );
+  }
+}
+
+export default ContactUs;
